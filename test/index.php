a5c12306ad688b4116a1976293053a1d6f092f43
= <wallacemaxters@gmail.com>
2016-02-17 18:03:44 -0200

    improve config factory loader

--- test/index.php
+++ test/index.php
@@ -9,14 +9,51 @@ use PHPLegends\Assets\Collections\CssCollection;
 use PHPLegends\Assets\Assets;
 
 
-$manager = Assets::create()
-                 ->getManager()
-                 ->addNamespace('home', 'assets')
-                 ->setBaseUri('http://assets.cardvantagens.com')
-                 ->add('js', 'home:*')
-                 ->add('js', 'home:admin/*')
-                 ->add('css', 'admin/teste');
+// $manager = Assets::create()
+//                  ->getManager()
+//                  ->addNamespace('home', 'assets')
+//                  ->setBaseUri('http://assets.cardvantagens.com')
+//                  ->add('js', 'home:*')
+//                  ->add('js', 'home:admin/*')
+//                  ->add('css', 'admin/teste');
 
-//print_r($manager);
+// //print_r($manager);
 
-echo $manager->output();
+// echo $manager->output();
+
+$config = [
+
+    'base' => 'http://localhost:8000/assets',
+
+    'css' => [
+
+        'namespaces' => [
+            'u.editors' => 'css/user/editors',
+            'u.posters' => 'css/user/posters'
+        ]
+    ],
+
+    'js' => [
+        'namespaces' => [
+            'admin' => 'js/admin'
+        ],
+
+        'add' => [
+            'admin:default.js'
+        ]   
+    ]
+];
+
+
+$assets = Assets::factory($config);
+
+$assets->add('js', 'admin:site');
+
+$assets->add('js', 'admin:site');
+
+
+$assets->add('css', 'u.editors:index');
+
+$assets->add('css', 'u.posters:post');
+
+echo $assets->output();
