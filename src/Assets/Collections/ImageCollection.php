a8be7a9e730f0ae5465d7e5fa547fe916d830a0d
= <wallacemaxters@gmail.com>
2016-02-23 16:54:57 -0300

    removing tabs

--- src/Assets/Collections/ImageCollection.php
+++ src/Assets/Collections/ImageCollection.php
@@ -5,22 +5,22 @@ namespace PHPLegends\Assets\Collections;
 class ImageCollection extends AbstractCollection
 {
 
-	public function getAssetAlias()
-	{
-		return 'img';
-	}
+    public function getAssetAlias()
+    {
+        return 'img';
+    }
 
-	public function buildTag($asset)
-	{
-		$attributes = ['src' => $asset] + $this->getAttributes();
+    public function buildTag($asset)
+    {
+        $attributes = ['src' => $asset] + $this->getAttributes();
 
-		$attr = $this->createHtmlAttributes($attributes);
+        $attr = $this->createHtmlAttributes($attributes);
 
-		return "<img {$attr}/>";
-	}
+        return "<img {$attr}/>";
+    }
 
-	public function getExtensions()
-	{
-		return ['jpg', 'png', 'bmp', 'jpeg'];
-	}
+    public function getExtensions()
+    {
+        return ['jpg', 'png', 'bmp', 'jpeg'];
+    }
 }
--- src/Assets/Collections/JavascriptCollection.php
+++ src/Assets/Collections/JavascriptCollection.php
@@ -4,24 +4,24 @@ namespace PHPLegends\Assets\Collections;
 
 class JavascriptCollection extends AbstractCollection
 {
-	protected $attributes = ['type' => 'text/javascript'];
+    protected $attributes = ['type' => 'text/javascript'];
 
-	public function getAssetAlias()
-	{
-		return 'js';
-	}
+    public function getAssetAlias()
+    {
+        return 'js';
+    }
 
-	public function buildTag($asset)
-	{
-		$attributes = ['src' => $asset] + $this->getAttributes();
+    public function buildTag($asset)
+    {
+        $attributes = ['src' => $asset] + $this->getAttributes();
 
-		$attr = $this->createHtmlAttributes($attributes);
+        $attr = $this->createHtmlAttributes($attributes);
 
-		return "<script {$attr}></script>";
-	}
+        return "<script {$attr}></script>";
+    }
 
-	public function getExtensions()
-	{
-		return ['js'];
-	}
+    public function getExtensions()
+    {
+        return ['js'];
+    }
 } 
